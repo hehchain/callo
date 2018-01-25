721f8b796cbf2b1e8d886406178fd7533c239762
Armel EMBOLO <feavfeav@gmail.com>
2018-01-25 11:25:25 +0100

    update interface of logic node

--- src/lib/logic_node.cpp
+++ src/lib/logic_node.cpp
@@ -3,3 +3,17 @@
 #include <stdlib.h>
 #define MAX_NEIGHBOUR 10
 #include "logic_node.h"
+struct HEADER{
+        int chainId;
+        int height;
+        time_t time;
+        char * lastBlockHash;
+        char * lastBlockParts;
+        char * stateHash;
+    };
+struct node
+{
+    HEADER header;
+    DATA data;
+    struct node * next;
+};
--- src/lib/logic_node.h
+++ src/lib/logic_node.h
@@ -20,6 +20,9 @@
 
 
 	typedef struct node *LogicNode;
+	typedef struct data{
+		char * data;
+	} *DATA;
 
 	/**
 	    Create empty logic's node.
@@ -29,48 +32,149 @@
 	LogicNode createLogicicNode();
 
 	/**
-	    Set name of logic node.
+	    Create root fixed logic's node.
+	    @param void
+	    @return LogicNode.
+	*/
+	LogicNode createRootLogicicNode();
+
+	/**
+	    Get previous logic's node.
+	    @param void
+	    @return LogicNode.
+	*/
+	LogicNode previousLNode();
+
+	/**
+	    Get next logic's node.
+	    @param void
+	    @return LogicNode.
+	*/
+	LogicNode nextLNode();
+
+	/**
+	 * ************************************************************************
+	 * 					SETTER METHODS OF HEADER OG LOGIC NODE				   *
+	 * ************************************************************************
+	 **/
+
+	/**
+	    Set if of logic node in chain.
 	    @param logic node(LogicNode *) and chain id to assign (char *)
 	    @return void.
 	*/
 	void setLNodeChainId(LogicNode *, const char *);
 
 	/**
-	    Set ip of logic node.
+	 * Not userful
+	    Set heigth of logic node.
 	    @param logic node(LogicNode *) and height to assign (int)
 	    @return void.
 	*/
 	void setLNodeHeight(LogicNode *, int);
 
 	/**
-	    Set port of communication of logic node.
+	 * may be set only during create node
+	    Set time of created node logic node.
 	    @param logic node(LogicNode *) and Time to assign (char *)
 	    @return void.
 	*/
 	void setLNodeTime(LogicNode *, time_t);
 
 	/**
-	    Set protocol using for communication of logic node.
+	    Set hash of previous logic node.
 	    @param logic node(LogicNode *) and LastBlockHash to assign (char)
 	    @return void.
 	*/
-	void setLNodeLastBlockHash(LogicNode *, const byte *);
+	void setLNodeLastBlockHash(LogicNode *, const char *);
 
 	/**
-	    Set public key using for communication with another logic node.
+	    Set header informations of last node logic node.
 	    @param logic node(LogicNode *) and part's header of last block to assign (char *)
 	    @return void.
 	*/
 	void setLNodeLastBlockParts(LogicNode *, const char *);
 
 	/**
-	    Set public key using for communication with another logic node.
+	    Set hash of current logic node.
 	    @param logic node(LogicNode *) and hash of current block transform to assign (char *)
 	    @return void.
 	*/
-	void setLNodeStateHash(LogicNode *, const byte *);
+	void setLNodeStateHash(LogicNode *, const char *);
+
+	/**
+	 * ************************************************************************
+	 * 					GETTER METHODS OF HEADER OG LOGIC NODE				  *
+	 * ************************************************************************
+	 **/
+
+	/**
+	    Get id of logic node in chain.
+	    @param logic node(LogicNode *)
+	    @return const char*.
+	*/
+	const char* getLNodeChainId(LogicNode *);
+
+	/**
+	    Get height of logic node.
+	    @param logic node(LogicNode *)
+	    @return int.
+	*/
+	int getLNodeHeight(LogicNode *);
+
+	/**
+	    Get Time of save logic node.
+	    @param logic node(LogicNode *)
+	    @return time_t.
+	*/
+	time_t getLNodeTime(LogicNode *);
+
+	/**
+	    Get hash of previous block logic's node.
+	    @param logic node(LogicNode *) 
+	    @return const char *.
+	*/
+	const char * setLNodeLastBlockHash(LogicNode *);
+
+	/**
+	    Get the last block header stringify information.
+	    @param logic node(LogicNode *)
+	    @return const char *.
+	*/
+	const char * setLNodeLastBlockParts(LogicNode *);
+
+	/**
+	    Get hash of current  logic node.
+	    @param logic node(LogicNode *)
+	    @return const char *.
+	*/
+	const char * setLNodeStateHash(LogicNode *);
+
+	/**
+	 * ************************************************************************
+	 * 					SETTER METHODS OF BODY OG LOGIC NODE				   *
+	 * ************************************************************************
+	 **/
+
+	/**
+	    Set data of logic node in chain.
+	    @param logic node(LogicNode *) and chain id to assign (char *)
+	    @return void.
+	*/
+	void setLNodeData(LogicNode *, DATA);
 
 
+	/**
+	 * ************************************************************************
+	 * 					GETTER METHODS OF BODY OG LOGIC NODE				  *
+	 * ************************************************************************
+	 **/
 
+	/**
+	    Get data of logic node in chain.
+	    @param logic node(LogicNode *)
+	    @return const char*.
+	*/
+	const DATA getLNodeData(LogicNode *);
 
 #endif
